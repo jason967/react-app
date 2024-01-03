d6efba8fe64cceca3dce54f2f92743c86101f1ac
choi-jae-woong <jason967@naver.com>
2024-01-04 01:13:43 +0900

    header

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,9 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 import "./App.css";
 
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Home from "./pages/Home";
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
     <Router>
       <Routes>
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,11 +1,19 @@
-import React from "react";
 import styled from "@emotion/styled";
 import main_logo from "../assets/svg/Logo.svg";
 
 export default function Header() {
   return (
     <HeaderWrapper>
-      <img src={main_logo} />
+      <TopBar>
+        <Logo src={main_logo} />
+        <Action>
+          <UserName>User Name</UserName>
+          <Divider />
+          <Button>개인정보 수정</Button>
+          <Divider />
+          <Button>로그아웃</Button>
+        </Action>
+      </TopBar>
     </HeaderWrapper>
   );
 }
@@ -19,10 +27,64 @@ const HeaderWrapper = styled.header`
   align-items: flex-start;
   flex-shrink: 0;
   align-self: stretch;
+
+  border-bottom: 1px solid var(--CoolGray-CoolGray100, #e6eaec);
+  background: var(--TrueGray-White, #fff);
+`;
+
+const TopBar = styled.ul`
+  background-color: aqua;
+  display: flex;
+  height: 36px;
+  /* max-width: var(--ScreenMaxWidth, 1600px); */
+  padding: 0px 8px;
+  justify-content: space-between;
+  align-items: center;
+  flex-shrink: 0;
+  align-self: stretch;
+`;
+
+const Logo = styled.img<{ src: string }>`
+  width: 88px;
+  height: 24px;
+  flex-shrink: 0;
+  src: ${({ src }) => src};
 `;
 
 const Action = styled.ul`
   display: flex;
-  justify-content: center;
+  padding: 0px var(--Radius_LG, 8px);
+  justify-content: flex-end;
   align-items: center;
+  gap: var(--Radius_XL, 16px);
+`;
+
+const Button = styled.button`
+  border: none;
+  background-color: transparent;
+  padding: 0;
+
+  color: var(--CoolGray-CoolGray700, #4e5962);
+  /* H5 */
+  font-family: Pretendard;
+  font-size: 14px;
+  font-style: normal;
+  font-weight: 700;
+  line-height: 20px; /* 142.857% */
+`;
+
+const UserName = styled.p`
+  color: var(--CoolGray-CoolGray700, #4e5962);
+  text-align: right;
+  /* Body2 */
+  font-family: Pretendard;
+  font-size: 14px;
+  font-style: normal;
+  font-weight: 400;
+  line-height: 20px; /* 142.857% */
+`;
+
+const Divider = styled.div`
+  height: 12px;
+  border-left: 1px solid var(--CoolGray-CoolGray200);
 `;
